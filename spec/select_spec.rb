9c9b489fbb547366e4fa2fa6847b5bd4a5da00db
mwagg <michael@guerillatactics.co.uk>
2010-01-06 00:39:54 +0000

    Renamed some variables in tests

--- spec/select_spec.rb
+++ spec/select_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 
 describe Select do
   it "should throw an exception if the element is not a select element" do
-    an_anchor = mock(:an_anchor)
-    an_anchor.should_receive(:tag_name).and_return('a')
-    running { Select.new(an_anchor)}.should raise_error UnexpectedTagNameError
+    anchor_element = mock(:an_anchor)
+    anchor_element.should_receive(:tag_name).and_return('a')
+    running { Select.new(anchor_element)}.should raise_error UnexpectedTagNameError
 
     a_select = mock(:an_anchor)
     a_select.should_receive(:tag_name).and_return('select')
@@ -14,50 +14,50 @@ describe Select do
   end
 
   it "should indicate that a select can support multiple options" do
-    a_multiple_select = mock(:select)
-    a_multiple_select.stub(:tag_name).and_return('select')
-    a_multiple_select.should_receive(:attribute).with('multiple').and_return('multiple')
+    select_element = mock(:select)
+    select_element.stub(:tag_name).and_return('select')
+    select_element.should_receive(:attribute).with('multiple').and_return('multiple')
 
-    select = Select.new(a_multiple_select)
+    select = Select.new(select_element)
     select.multiple?.should be_true
   end
 
   it "should not indicate that a normal select supports multiple options" do
-    a_normal_select = mock(:select)
-    a_normal_select .stub(:tag_name).and_return('select')
-    a_normal_select.should_receive(:attribute).with('multiple').and_raise(Selenium::WebDriver::Error::WebDriverError)
+    select_element = mock(:select)
+    select_element .stub(:tag_name).and_return('select')
+    select_element.should_receive(:attribute).with('multiple').and_raise(Selenium::WebDriver::Error::WebDriverError)
 
-    select = Select.new(a_normal_select)
+    select = Select.new(select_element)
     select.multiple?.should be_false
   end
 
   it "should return all options when asked" do
-    a_select = mock(:select)
+    select_element = mock(:select)
     options = []
 
-    a_select.stub(:tag_name).and_return('select')
-    a_select.stub(:attribute?).with('multiple').and_return('multiple')
-    a_select.should_receive(:find_elements).with(:tag_name, 'option').and_return(options)
+    select_element.stub(:tag_name).and_return('select')
+    select_element.stub(:attribute?).with('multiple').and_return('multiple')
+    select_element.should_receive(:find_elements).with(:tag_name, 'option').and_return(options)
 
-    select = Select.new(a_select)
+    select = Select.new(select_element)
     returned_options = select.options
 
     returned_options.should equal(options)
   end
 
   it "should return options which are selected" do
-    a_select = mock(:select)
+    select_element = mock(:select)
     good_option = mock(:good_option)
     bad_option = mock(:bad_option)
     options = [bad_option, good_option]
 
-    a_select.stub(:tag_name).and_return('select')
-    a_select.stub(:attribute?).with('multiple').and_return('multiple')
-    a_select.should_receive(:find_elements).with(:tag_name, 'option').and_return(options)
+    select_element.stub(:tag_name).and_return('select')
+    select_element.stub(:attribute?).with('multiple').and_return('multiple')
+    select_element.should_receive(:find_elements).with(:tag_name, 'option').and_return(options)
     bad_option.should_receive(:selected?).and_return(false)
     good_option.should_receive(:selected?).and_return(true)
 
-    select = Select.new(a_select)
+    select = Select.new(select_element)
     returned_options = select.selected_options
 
     returned_options.length.should == 1
